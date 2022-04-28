6d807fa28d022f02700338f8518baa97809e4b5c
eloygm92 <eloygm.92@uma.es>
2022-04-28 20:33:12 +0200

    Authentication done?

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,9 +1,8 @@
-import {Injectable, UnauthorizedException} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { jwtConstants } from '../constants';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { User } from '../../user/schemas/user.schema';
-import {UserService} from "../../user/user.service";
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -15,16 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  /*async validate(payload: any) {
+  async validate(payload: any) {
     return { userId: payload.sub, username: payload.username };
-  }*/
-  async validate(payload: any): Promise<User> {
-    const { username } = payload;
-    const user = await this.userService.findOne({ username });
-
-    if (!user) {
-      throw new UnauthorizedException();
-    }
-    return user;
   }
 }
